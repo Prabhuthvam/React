6d6580220d3f88a386fc819d458bc9fce30343e2
Prabhuthvam <98016401+Prabhuthvam@users.noreply.github.com>
2023-11-22 12:41:22 +0530

    Update mapcomponent.jsx

--- Nov 22 Day 3/using_map_to_create_table/Component/mapcomponent.jsx
+++ Nov 22 Day 3/using_map_to_create_table/Component/mapcomponent.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-class mapcomponent extends Component {
-    state = {  } 
-    render() { 
-        return (<table class="table">
+
+class MapComponent extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        firstName: 'Prabhuthvam',
+        lastName: 'Chakali',
+        address: 'Shadnagar',
+      },
+      {
+        id: 2,
+        firstName: 'Ram',
+        lastName: 'Seetha',
+        address: 'Bharath',
+      },
+      {
+        id: 3,
+        firstName: 'Suriya',
+        lastName: 'Hero',
+        address: 'Chennai',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">Id</th>
@@ -13,27 +37,18 @@ class mapcomponent extends Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Prabhuthvam</td>
-            <td>Chakali</td>
-            <td>Shadnagar</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Ram</td>
-            <td>Seetha</td>
-            <td>Bharath</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Suriya</td>
-            <td>Hero</td>
-            <td>Chennai</td>
-          </tr>
+          {this.state.users.map((user) => (
+            <tr key={user.id}>
+              <th scope="row">{user.id}</th>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.address}</td>
+            </tr>
+          ))}
         </tbody>
-      </table>);
-    }
+      </table>
+    );
+  }
 }
- 
-export default mapcomponent;
+
+export default MapComponent;
