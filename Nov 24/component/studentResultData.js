8845a6d7bded9b1601483f529f3deb4d01441b09
Prabhuthvam <98016401+Prabhuthvam@users.noreply.github.com>
2023-11-27 19:34:10 +0530

    Add files via upload

---
+++ Nov 24/component/studentResultData.js
@@ -0,0 +1,96 @@
+const StudentsData = [
+    {
+        "rollno": 100,
+        "name": "Chiranjeevi",
+        "telugu": 100,
+        "hindi": 89,
+        "english": 80,
+        "maths": 60,
+        "science": 86,
+        "social": 90
+    },
+    {
+        "rollno": 101,
+        "name": "Suriya",
+        "telugu": 88,
+        "hindi": 90,
+        "english": 92,
+        "maths": 60,
+        "science": 85,
+        "social": 90
+    },
+    {
+        "rollno": 102,
+        "name": "Mahesh Babu",
+        "telugu": 95,
+        "hindi": 88,
+        "english": 90,
+        "maths": 34,
+        "science": 85,
+        "social": 92
+    },
+    {
+        "rollno": 103,
+        "name": "Allu Arjun",
+        "telugu": 92,
+        "hindi": 20,
+        "english": 91,
+        "maths": 29,
+        "science": 93,
+        "social": 87
+    },
+    {
+        "rollno": 104,
+        "name": "Prabhas",
+        "telugu": 87,
+        "hindi": 91,
+        "english": 88,
+        "maths": 82,
+        "science": 90,
+        "social": 85
+    },
+    {
+        "rollno": 105,
+        "name": "NTR ",
+        "telugu": 90,
+        "hindi": 87,
+        "english": 92,
+        "maths": 85,
+        "science": 88,
+        "social": 91
+    },
+    {
+        "rollno": 106,
+        "name": "Ram Charan",
+        "telugu": 91,
+        "hindi": 86,
+        "english": 89,
+        "maths": 90,
+        "science": 87,
+        "social": 92
+    },
+    {
+        "rollno": 107,
+        "name": "Vijay Deverakonda",
+        "telugu": 94,
+        "hindi": 85,
+        "english": 90,
+        "maths": 25,
+        "science": 89,
+        "social": 93
+    },
+    {
+        "rollno": 108,
+        "name": "Pawan Kalyan",
+        "telugu": 88,
+        "hindi": 89,
+        "english": 34,
+        "maths": 20,
+        "science": 86,
+        "social": 90
+    }
+];
+
+export function getStudentsData() {
+    return StudentsData;
+}
