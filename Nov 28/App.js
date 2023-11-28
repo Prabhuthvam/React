c30ae34242e630934c8ba28f33d2339d597d4f75
Prabhuthvam <98016401+Prabhuthvam@users.noreply.github.com>
2023-11-28 22:54:56 +0530

    Add files via upload

---
+++ Nov 28/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Pagination from './component/pagination';
+import 'bootstrap/js/dist/base-component';
+
+function App() {
+  return (
+    <Pagination></Pagination>
+  );
+}
+
+export default App;
